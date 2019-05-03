fbce754d5cf26d56a251be52d29ff678f345c771
Armaan Hossain <43430610+ArmaanJH@users.noreply.github.com>
2019-05-03 00:04:54 -0400

    Create main.cpp
    Beginning stage of creative exploration

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode* left; //points to left child
+    TreeNode* right; //points to right child
+    TreeNode(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main(){
+
+  return 0;
+}
